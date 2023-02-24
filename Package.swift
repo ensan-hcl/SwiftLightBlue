f49d44b7c2ffaace848eeb495cc9abd296ae33fe
ensan <ensan.hcl.dev@gmail.com>
2023-02-24 09:02:59 +0900

    フォルダ構造を元実装と揃えた

--- Package.swift
+++ Package.swift
@@ -23,7 +23,9 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SwiftLightBlue",
-            dependencies: []),
+            dependencies: [],
+            resources: [.copy("Japanese/Juman.dic.tsv")]
+        ),
         .testTarget(
             name: "SwiftLightBlueTests",
             dependencies: ["SwiftLightBlue"]),
